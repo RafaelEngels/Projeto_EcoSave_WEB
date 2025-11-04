f2005b2e5405565d1648f9e1518583420ee2ee3b
RafaelEngels <rafaelengel30@gmail.com>
2025-11-04 19:28:52 -0300

    main files, null

---
+++ php/realizar_denuncias.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Inicial</title>
+</head>
+<body>
+    Cadastre-se na Página!
+    <form id="formulario">
+        <input type="text" name="nome" placeholder="Digite Seu Nome!">
+    </form>
+</body>
+</html>
